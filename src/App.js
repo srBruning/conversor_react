6f94dcc23cb4a4ed95445d68106ac446cb5c3db6
Diego Bruning <diego.brg92@gmail.com>
2021-12-07 22:34:05 -0300

    conversor react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Conversor from "./components/Conversor";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>Conversor de Moedas</h1>
+    <div className="linha">
+      <Conversor moedaA="USD" moedaB="BRL" />
+      <Conversor moedaA="BRL" moedaB="USD" />
+    </div>
+
+    <div className="linha">
+        <Conversor moedaA="CAD" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="CAD" />
+      </div>
+
+      <div className="linha">
+        <Conversor moedaA="EUR" moedaB="BRL" />
+        <Conversor moedaA="BRL" moedaB="EUR" />
+      </div>
     </div>
   );
 }
